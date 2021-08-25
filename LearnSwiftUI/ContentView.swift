c831e578d3644887ee1b528f642424de80876ed4
Rasyid <rasydridla298@gmail.com>
2021-08-25 09:56:17 +0700

    StackView (Vstack, Hstack, Zstack ) #2

--- LearnSwiftUI/ContentView.swift
+++ LearnSwiftUI/ContentView.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HandView()
+        ZStack{
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            VStack(spacing: 30){
+                Logo()
+                FormBox()
+            }
+            .padding(.all, 20)
+        }
     }
 }
 
@@ -21,16 +31,61 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-
-struct HandView : View {
+struct Logo : View {
     var body: some View{
-        Image(systemName: "hand.raised")
-            .resizable()
-            .frame(width: 100, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: .center)
-            .background(Color("blue"))
-            .foregroundColor(Color.white)
+        
+        VStack{
+            Image("swift")
+                .resizable()
+                .frame(width: 100, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: .center)
+                .background(Color("blue"))
+                .foregroundColor(Color.white)
+                .padding()
+                .background(Color("blue"))
+                .cornerRadius(20)
+            
+            Text("Hello SwiftUI")
+                .foregroundColor(Color.white)
+        }
+        
+    }
+}
+
+struct FormBox : View{
+    
+    @State var username: String = ""
+    @State var password: String = ""
+    
+    var body: some View{
+        VStack(alignment:.leading){
+            
+//            tf username
+            Text("username").font(.callout).bold()
+            TextField("username..", text: $username)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+//            tf password
+            Text("password").font(.callout).bold()
+            SecureField("password", text: $password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+//            btn
+            Button(action: {print("Hello World")}, label: {
+                HStack{
+                    Text("Sign In")
+                    Spacer()
+                }
+            })
             .padding()
-            .background(Color("blue"))
-            .cornerRadius(20)
+            .background(Color.black)
+            .cornerRadius(10)
+            .foregroundColor(Color.white)
+        }
+        .padding(.all, 30)
+            .background(Color.orange)
+            .cornerRadius(10)
+    }
 }
-}
+
+
+
